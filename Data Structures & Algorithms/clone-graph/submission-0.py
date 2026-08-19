97305532911a920be979b15a4380de392e49e638
Michael Boateng <112430340+michaelboat@users.noreply.github.com>
2026-08-19 00:26:51 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
+        visited = {}
+
+        def helper(node):
+            if not node:
+                return node
+
+            if node in visited:
+                return visited[node]
+
+            cloned_node = Node(node.val, [])
+            visited[node] = cloned_node
+
+            if node.neighbors:
+                cloned_node.neighbors = [helper(n) for n in node.neighbors]
+
+            return cloned_node
+
+
+        return helper(node)
+        
+        
